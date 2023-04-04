7295fab1937a5726b2431fd89dfd7c4f3475eb6b
itguy19 <125930481+itguy19@users.noreply.github.com>
2023-04-04 10:00:56 +0200

    Delete test.py

--- test.py
+++
@@ -1,7 +0,0 @@
-from APIConnections import get_device_uuid, send_sensor_data
-import calendar
-import time
-
-current_GMT = time.gmtime()
-ts = calendar.timegm(current_GMT)
-send_sensor_data(get_device_uuid()[0], 1678035029 * 1000, 23, 23, 23, 43, 43)
